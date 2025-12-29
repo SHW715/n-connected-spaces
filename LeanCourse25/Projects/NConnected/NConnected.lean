b15c9b3a0994d6044ff1b1dcfc25c894f7df41c4
SHW715 <ShuhanWang413@outlook.com>
2025-12-29 22:25:14 +0100

    Set up lean file

---
+++ LeanCourse25/Projects/NConnected/NConnected.lean
@@ -0,0 +1,14 @@
+-- Project 2: n-connected spaces
+-- Author: Shuhan Wang, Wenrong Zou
+
+-- Definition of homotopy fiber of a (continuous) map
+
+
+
+
+
+
+
+
+
+-- Definitions of n-connected maps and n-connected spaces
